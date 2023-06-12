5b7f38d04a775a183d04767d36db06275d0c80f9
Raphael Bottino <raphabot@gmail.com>
2023-06-12 14:27:37 -0500

    Updating Diagram

--- diagram/diagram.py
+++ diagram/diagram.py
@@ -1,9 +1,18 @@
 # diagram.py
-from diagrams import Diagram
+from diagrams import Cluster, Diagram
 from diagrams.aws.compute import LambdaFunction
 from diagrams.aws.integration import Eventbridge
 from diagrams.aws.storage import SimpleStorageServiceS3BucketWithObjects, SimpleStorageServiceS3Object
 from diagrams.saas.social import Twitter
+from diagrams.custom import Custom
 
 with Diagram("Daily AWS Tips", filename="diagram", outformat=["svg", "png"], show=False):
-    Eventbridge("Once, every day at 10AM CST") >> LambdaFunction("PickTipsLambda") >> SimpleStorageServiceS3Object("tip-of-the-day.json") >> SimpleStorageServiceS3BucketWithObjects("TipsBucket") >> LambdaFunction("TweetTipLambda") >> Twitter("@DailyAwsTips")
+    bucketEvent = SimpleStorageServiceS3BucketWithObjects("TipsBucket")
+
+    Eventbridge("Once, every day at 10AM CST") >> LambdaFunction("PickTipsLambda") >> SimpleStorageServiceS3Object("tip-of-the-day.json") >> bucketEvent
+
+    with Cluster("Twitter"):
+        bucketEvent >> LambdaFunction("TweetTipLambda") >> Twitter("@DailyAwsTips")
+
+    with Cluster("Mastodon"):
+        bucketEvent >> LambdaFunction("Mastodon") >> Custom("@DailyAwsTips@mastodon.online", "./mastodon-icon.png")
